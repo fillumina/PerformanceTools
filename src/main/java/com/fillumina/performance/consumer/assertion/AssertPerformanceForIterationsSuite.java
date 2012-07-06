f35b5bbdcb15f3643d7caf17909d63aae96d75bf
Francesco Illuminati <fillumina@gmail.com>
2012-07-06 20:46:46 +0200

    added: created an Assertion for iteration and an example to better explain ProgressionSequence

---
+++ src/main/java/com/fillumina/performance/consumer/assertion/AssertPerformanceForIterationsSuite.java
@@ -0,0 +1,31 @@
+package com.fillumina.performance.consumer.assertion;
+
+import com.fillumina.performance.consumer.AbstractPerformanceConsumer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author fra
+ */
+public class AssertPerformanceForIterationsSuite
+        extends AbstractPerformanceConsumer<AssertPerformanceForIterationsSuite> {
+    private static final long serialVersionUID = 1L;
+
+    private Map<Long, AssertPerformance> map = new HashMap<>();
+
+    public AssertPerformance addAssertionForIterations(final long iterations) {
+        final AssertPerformance assertPerformance = new AssertPerformance();
+        map.put(iterations, assertPerformance);
+        return assertPerformance;
+    }
+
+    @Override
+    public void process() {
+        final long iterations = getLoopPerformances().getIterations();
+        final AssertPerformance assertPerformance = map.get(iterations);
+        if (assertPerformance != null) {
+            assertPerformance.process();
+        }
+    }
+}
---
+++ src/test/java/com/fillumina/performance/producer/sequence/ProgressionSequenceExampleTest.java
@@ -0,0 +1,99 @@
+package com.fillumina.performance.producer.sequence;
+
+import com.fillumina.performance.PerformanceTimerBuilder;
+import com.fillumina.performance.consumer.assertion.AssertPerformanceForIterationsSuite;
+import com.fillumina.performance.producer.timer.PerformanceTimer;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * This is an example of a performance test that starts being favorable to
+ * one test and ends, after being favorable to the other. In this
+ * case the JVM optimizations have nothing to do with that but it's easy
+ * to understand how JVM optimizations could impact performances in
+ * the same way.
+ *
+ * @author fra
+ */
+public class ProgressionSequenceExampleTest {
+    public static final double INCREMENT = Math.PI / 128;
+    public static final double PERIOD = Math.PI * 2;
+
+    private static class CachedSin {
+        private final Map<Double, Double> map = new HashMap<>();
+
+        private double sin(final double value) {
+            double x = value % PERIOD;
+            Double y = map.get(x);
+            if (y == null) {
+                y = Math.sin(x);
+                map.put(x, y);
+            }
+            return y;
+        }
+    }
+
+    @Test
+    public void shoudPerformanceChangeWithIterations() {
+        final PerformanceTimer pt = PerformanceTimerBuilder.createSingleThread();
+
+        pt.addTest("direct", new Runnable() {
+            double x = 0;
+
+            @Override
+            public void run() {
+                assertTrue(Math.sin(x) != 2);
+                x = increment(x);
+            }
+
+        });
+
+        pt.addTest("cached", new Runnable() {
+            final CachedSin cachedSin = new CachedSin();
+            double x = 0;
+
+            @Override
+            public void run() {
+                assertTrue(cachedSin.sin(x) != 2);
+                x = increment(x);
+            }
+        });
+
+        final AssertPerformanceForIterationsSuite assertionSuite =
+                createAssertionForIterationsSuite();
+
+        new ProgressionSequence(pt)
+                .setBaseTimes(1000)
+                .setMaximumMagnitude(3)
+                .setSamplePerMagnitude(10)
+                .setOnIterationPerformanceConsumer(assertionSuite)
+                .executeSequence();
+
+    }
+
+    private AssertPerformanceForIterationsSuite createAssertionForIterationsSuite() {
+        final AssertPerformanceForIterationsSuite assertionSuite =
+                new AssertPerformanceForIterationsSuite();
+
+        assertionSuite.addAssertionForIterations(1_000)
+                .assertTest("cached").slowerThan("direct");
+
+        assertionSuite.addAssertionForIterations(10_000)
+                .assertTest("cached").slowerThan("direct");
+
+        assertionSuite.addAssertionForIterations(100_000)
+                .assertTest("cached").fasterThan("direct");
+
+        return assertionSuite;
+    }
+
+    private static double increment(final double x) {
+        double res = x + INCREMENT;
+        if (res > PERIOD) {
+            res = 0;
+        }
+        return res;
+    }
+}
