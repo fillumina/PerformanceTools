7c473ebf93e4be22b7da33a4fc346d7cd3ed098c
Francesco Illuminati <fillumina@gmail.com>
2012-07-27 18:02:49 +0200

    added: test to check the timer used in tests

---
+++ src/test/java/com/fillumina/performance/utils/PerformanceTimeHelperTest.java
@@ -0,0 +1,30 @@
+package com.fillumina.performance.utils;
+
+import org.junit.Test;
+import static com.fillumina.performance.utils.PerformanceTimeHelper.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fra
+ */
+public class PerformanceTimeHelperTest {
+
+    @Test
+    public void shouldBePrecise() {
+        assertElapsedMillis(10);
+        assertElapsedMillis(20);
+        assertElapsedMillis(100);
+        assertElapsedMillis(200);
+        assertElapsedMillis(500);
+        assertElapsedMillis(1_000);
+    }
+
+    private void assertElapsedMillis(final int millis) {
+        final long time = System.currentTimeMillis();
+        sleepMicroseconds(millis * 1_000);
+        final long elapsed = System.currentTimeMillis() - time;
+
+        assertEquals(millis, elapsed, 0.1);
+    }
+}
